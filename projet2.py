ae56e6c146475da6f11ff0c16b28b18dd0eb0970
Anakin512Vador <64581200+Anakin512Vador@users.noreply.github.com>
2020-05-04 20:44:10 +0200

    Update projet2.py
    ajout de la fonction 'read' (elle doit être dans le fichier mère)

--- projet2.py
+++ projet2.py
@@ -1,3 +1,19 @@
-from Zlan import*
-from tkinter import*
 from moduleprojet2 import*
+from tkinter import*
+from csv import*
+
+def read(nom_du_fichier):
+    assert type(nom_du_fichier)!='str'
+    fichier=open("listefichier.csv", encoding='utf8')
+    tableau = list(DictReader(fichier))
+    fichier.close()
+    liste_fichier =[elt['liste_des_fichier'] for elt in tableau if elt['liste_des_fichier'] == nom_du_fichier]
+    if len(liste_fichier) == 0:
+        return
+    else :
+        fichier=open(nom_du_fichier, encoding='utf8')
+        tableau = list(DictReader(fichier,delimiter=";"))
+        fichier.close()
+        return tableau
+
+print(read('1nsi.csv'))
